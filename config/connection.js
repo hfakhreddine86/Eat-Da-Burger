fe4b73f2e03f673be98d7b1b465c7fe8d603b535
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-02-26 12:19:29 -0800

    testing

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     port: 3306,
     host: 'localhost',
-    user: 'test',
-    password: 'test123',
+    user: 'root',
+    password: '',
     database: 'burgers_db'
   });
 }
